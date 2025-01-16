5082794acda1025dacee6ebf450e8c5f3dfce9e6
camerondhhs <cameron.tran@dhhs.vic.gov.au>
2025-01-16 15:07:54 +1100

    add get_nltk_sentiment

--- pandas_utilities/nltk_sentiment.py
+++ pandas_utilities/nltk_sentiment.py
@@ -15,3 +15,27 @@ def nltksentiment(comment: str ) -> dict:
         x = None
     return x
 
+# Use type-annotated function signature or function type hinting
+from typing import Dict, Any
+def get_nltk_sentiment(comment: str) -> Dict[str, Any]:
+    sentiment_scores = sid.polarity_scores(comment)
+    compound = sentiment_scores['compound']
+    neg = sentiment_scores['neg']
+    neu = sentiment_scores['neu']
+    pos = sentiment_scores['pos']
+
+    # Determine sentiment status
+    if compound >= 0.05:
+        sentiment = 'positive'
+    elif compound <= -0.05:
+        sentiment = 'negative'
+    else:
+        sentiment = 'neutral'
+
+    return {
+        'sentiment': sentiment,
+        'compound_score': compound,
+        'negative_score': neg,
+        'neutral_score': neu,
+        'positive_score': pos
+    }
