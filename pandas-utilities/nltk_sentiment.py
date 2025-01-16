0a3df6dea32a8c00987d9e751797f70d4f0ad424
camerondhhs <cameron.tran@dhhs.vic.gov.au>
2025-01-16 11:34:22 +1100

    Update nltk_sentiment.py

--- pandas-utilities/nltk_sentiment.py
+++ pandas-utilities/nltk_sentiment.py
@@ -1,1 +1,16 @@
+import pandas as pd
+import nltk
+import sklearn
+nltk.download('vader_lexicon')
 
+# Import SentimentIntensityAnalyzer and create an sid object
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+sid = SentimentIntensityAnalyzer()
+
+def ntlksentiment(comment: str ) -> dict:
+    print(comment)
+    if (isinstance(comment, str)):
+        x = sid.polarity_scores(comment)
+    else:
+        x = None
+    return x
