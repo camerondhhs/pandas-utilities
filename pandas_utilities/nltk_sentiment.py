1bebc9e8f22d0d38581bf10550b3b4c6326b46d8
camerondhhs <cameron.tran@dhhs.vic.gov.au>
2025-01-24 12:39:02 +1100

    add get sentiment label

--- pandas_utilities/nltk_sentiment.py
+++ pandas_utilities/nltk_sentiment.py
@@ -39,3 +39,20 @@ def get_nltk_sentiment(comment: str) -> Dict[str, Any]:
         'neutral_score': neu,
         'positive_score': pos
     }
+
+def get_nltk_sentiment_label(comment: str) -> str:
+    sentiment_scores = sid.polarity_scores(comment)
+    compound = sentiment_scores['compound']
+    neg = sentiment_scores['neg']
+    neu = sentiment_scores['neu']
+    pos = sentiment_scores['pos']
+
+    # Determine sentiment status
+    if compound >= 0.05:
+        sentiment = 'Positive'
+    elif compound <= -0.05:
+        sentiment = 'Negative'
+    else:
+        sentiment = 'Neutral'
+
+    return sentiment
